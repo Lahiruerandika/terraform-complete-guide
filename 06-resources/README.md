15915cd34b50a9a590cc95e2a98d04698d9234af
Lahiruerandika <erandikalahiru7@gmail.com>
2025-06-01 09:13:52 +0530

    06-resources

--- 06-resources/README.md
+++ 06-resources/README.md
@@ -2,7 +2,7 @@ Steps for implementation:
 1. Deploy a VPC and a subnet
 2. Deploy an internet gateway and associated it with the VPC
 3. Setup a route table with a route to the IGW
-4. Deploy an EC2 instance inside aof the created subnet
-5. Assoicated a public IP and a security group that allows public ingress
+4. Deploy an EC2 instance inside of the created subnet and assoicated a public IP 
+5. Associate a security group that allows public ingress
 6. Change the EC2 instance to use a publicly available NGINX AMI
 7. Destroy everything
--- 06-resources/compute.tf
+++ 06-resources/compute.tf
@@ -1,12 +1,37 @@
 resource "aws_instance" "web" {
-    ami = "put_relavent_ami_id_for_region"
-    associate_public_ip_address = true
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.public.id
-    root_block_device {
-      delete_on_termination = true
-      volume_size = 10
-      volume_type = "gp3"
-    }
-  
+  ami                         = "put_relavent_ami_id_for_region"
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
+
+}
+
+resource "aws_security_group" "public_http_traffic" {
+  description = "Security group allowing traffic on ports 443 and 80"
+  name        = "public-http-traffic"
+  vpc_id      = aws_vpc.main.id
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+
 }
