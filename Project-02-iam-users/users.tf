7ff7f5447bd1a400c7047d121dd2a71adca4d8b7
Lahiruerandika <erandikalahiru7@gmail.com>
2025-06-12 22:55:06 +0530

    Project-02-iam-users

--- Project-02-iam-users/users.tf
+++ Project-02-iam-users/users.tf
@@ -2,3 +2,7 @@ locals {
   users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml")).users
 }
 
+resource "aws_iam_user" "users" {
+  for_each = toset(local.users_from_yaml[*].username)
+  name     = each.value
+}
