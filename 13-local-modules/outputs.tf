d73f9283b31a1fb240ed78366cf61eac96ef46f3
Lahiruerandika <erandikalahiru7@gmail.com>
2025-07-08 21:55:33 +0530

    13-local-modules

--- 13-local-modules/outputs.tf
+++ 13-local-modules/outputs.tf
@@ -2,9 +2,7 @@ output "module_vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "module_public_subnets" {
-  value = module.vpc.public_subnets
-}
+
 
 output "module_private_subnets" {
   value = module.vpc.private_subnets
